d522af0fc831e3a94e85d453b0ac37b282db4ff2
MatteoTonelli05 <matteo.tonelli8@studio.unibo.it>
2024-11-13 11:08:52 +0100

    third

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -1,49 +1,126 @@
 package it.unibo.deathnote.impl;
 
+import java.sql.Time;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 import it.unibo.deathnote.api.DeathNote;
 
-public class DeathNoteImplementation implements DeathNote{
+public class DeathNoteImplementation implements DeathNote {
+    public class DeathData {
+        private String name;
+        private String cause;
+        private String details;
+
+        public DeathData(String name, String cause, String details) {
+            this.setName(name);
+            this.setCause(cause);
+            this.setDetails(details);
+        }
+
+        public DeathData(String name) {
+            this(name, null, null);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getCause() {
+            return cause;
+        }
+
+        public String getDetails() {
+            return details;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setCause(String cause) {
+            this.cause = cause;
+        }
+
+        public void setDetails(String details) {
+            this.details = details;
+        }
+    }
+
+    private List<DeathData> deaths;
+    long timeLastWrite;
+
+    public DeathNoteImplementation() {
+        deaths = new LinkedList<>();
+    }
 
     @Override
     public String getRule(int ruleNumber) {
-        // TODO Auto-generated method stub
-        throw new IllegalArgumentException("nicholas magi");
+        if (ruleNumber <= 0) {
+            throw new IllegalArgumentException("error: invalid argument");
+        } else {
+            return DeathNote.RULES.get(ruleNumber);
+        }
     }
 
     @Override
     public void writeName(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+        if (!this.isNameWritten(name)) {
+            deaths.add(new DeathData(name));
+            timeLastWrite = System.currentTimeMillis();
+        }
     }
 
     @Override
     public boolean writeDeathCause(String cause) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+        long tmp = System.currentTimeMillis();
+        if (timeLastWrite - tmp < 40) {
+            deaths.getLast().setCause(cause);
+            return true;
+        }
+        deaths.remove(deaths.getLast());
+        return false;
     }
 
     @Override
     public boolean writeDetails(String details) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
     }
 
     @Override
     public String getDeathCause(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+        Iterator<DeathData> iter = deaths.iterator();
+        while (iter.hasNext()) {
+            DeathData deathData = iter.next();
+            if (deathData.name.equals(name)) {
+                return deathData.cause;
+            }
+        }
+        return null;
     }
 
     @Override
     public String getDeathDetails(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+        Iterator<DeathData> iter = deaths.iterator();
+        while (iter.hasNext()) {
+            DeathData deathData = iter.next();
+            if (deathData.name.equals(name)) {
+                return deathData.details;
+            }
+        }
+        return null;
     }
 
     @Override
     public boolean isNameWritten(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+        Iterator<DeathData> iter = deaths.iterator();
+        while (iter.hasNext()) {
+            DeathData deathData = iter.next();
+            if (deathData.name.equals(name)) {
+                return true;
+            }
+        }
+        return false;
     }
-    
+
 }
