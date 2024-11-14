2afeb496866d6209cef0013aa6ef32c3cf7e45d8
MatteoTonelli05 <matteo.tonelli8@studio.unibo.it>
2024-11-14 18:37:07 +0100

    end

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,31 +1,72 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.Constructor;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
  * Application entry-point.
  */
 public final class LaunchApp {
 
-    private LaunchApp() { }
+    private LaunchApp() {
+    }
 
     /**
      * Runs the application.
      *
      * @param args ignored
-     * @throws ClassNotFoundException if the fetches class does not exist
-     * @throws NoSuchMethodException if the 0-ary constructor do not exist
+     * @throws ClassNotFoundException    if the fetches class does not exist
+     * @throws NoSuchMethodException     if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
-     * @throws InstantiationException if the constructor throws exceptions
-     * @throws IllegalAccessException in case of reflection issues
-     * @throws IllegalArgumentException in case of reflection issues
+     * @throws InstantiationException    if the constructor throws exceptions
+     * @throws IllegalAccessException    in case of reflection issues
+     * @throws IllegalArgumentException  in case of reflection issues
      */
+    private static final String[] VIEW_CLASS_NAMES = {
+            "it.unibo.mvc.view.PrintNumberView",
+            "it.unibo.mvc.view.PrintNumberView",
+            "it.unibo.mvc.view.PrintNumberView",
+            "it.unibo.mvc.view.PrintNumberView",
+            "it.unibo.mvc.view.PrintNumberView",
+            "it.unibo.mvc.view.PrintNumberView"
+    };
+
     public static void main(final String... args) {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
+
+        for (String viewClassName : VIEW_CLASS_NAMES) {
+            try {
+                // Load the class by name
+                Class<?> viewClass = Class.forName(viewClassName);
+
+                // Check if the class implements DrawNumberView interface
+                if (DrawNumberView.class.isAssignableFrom(viewClass)) {
+
+                    // Get the 0-argument constructor
+                    Constructor<?> constructor = viewClass.getConstructor();
+
+                    // Create an instance of the view
+                    DrawNumberView viewInstance = (DrawNumberView) constructor.newInstance();
+
+                    // Add the view to the controller
+                    app.addView(viewInstance);
+                } else {
+                    System.err.println("Class " + viewClassName + " does not implement DrawNumberView.");
+                }
+
+            } catch (Exception e) {
+                System.err.println("Failed to load view: " + viewClassName);
+                e.printStackTrace();
+            }
+        }
+
+        // Repeat the above loop for creating three command line views and three
+        // graphical views
+        // Here you might add each type multiple times if necessary
     }
 }
--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
@@ -4,6 +4,8 @@ import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -14,31 +16,29 @@ import java.util.Objects;
 public final class DrawNumberControllerImpl implements DrawNumberController {
 
     private final DrawNumber model;
-    private DrawNumberView view;
+    private List<DrawNumberView> view;
 
     /**
      * Builds a new game controller provided a game model.
-     *
+     * 
      * @param model the implementation of the game model
      */
     public DrawNumberControllerImpl(final DrawNumber model) {
         this.model = model;
+        view = new ArrayList<>();
     }
 
     @Override
     public void addView(final DrawNumberView view) {
         Objects.requireNonNull(view, "Cannot set a null view");
-        if (this.view != null) {
-            throw new IllegalStateException("The view is already set! Multiple views are not supported");
-        }
-        this.view = view;
+        this.view.add(view);
         view.setController(this);
         view.start();
     }
 
     @Override
     public void newAttempt(final int n) {
-        Objects.requireNonNull(view, "There is no view attached!").result(model.attempt(n));
+        Objects.requireNonNull(view.get(0), "There is no view attached!").result(model.attempt(n));
     }
 
     @Override
